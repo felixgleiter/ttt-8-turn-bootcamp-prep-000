99a5a12ae6a0090536b86fcd1f22972d2aa7324e
Felix Godfree <felixfgodfree@gmail.com>
2018-09-11 09:55:49 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,13 +7,6 @@ def display_board(board)
 end
 
 def valid_move?(board, index)
-  def position_taken?(board, index)
-    if board[index] == " " || board[index] == "" || board[index] == nil 
-      false
-    elsif board[index] == "X" || board[index] == "O"
-      true
-    end
-  end
   if index.between?(0, 8) 
     if !position_taken?(board, index)
       true 
